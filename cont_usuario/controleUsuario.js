36bb35e85abfd3b43efa67492841d075425741bc
Lindomar-lucietti <camillalucietti@gmail.com>
2024-08-27 20:34:58 -0300

    Tela frontend Inicial - Usuário e Admin
    Criação das duas telas inciais.

--- cont_usuario/controleUsuario.js
+++ cont_usuario/controleUsuario.js
@@ -10,4 +10,10 @@ router.get("/modificarsenha",(req,res)=>{
 router.get("/telausuario",(req,res)=>{
     res.render("pages/telausuario")
 })
+router.get("/inicialadmin",(req,res)=>{
+    res.render("pages/inicialadmin")
+})
+router.get("/inicialusuario",(req,res)=>{
+    res.render("pages/inicialusuario")
+})
 module.exports = router;
