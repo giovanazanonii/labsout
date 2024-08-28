1f1303d5e4084a38d1cbda4343cf1259078cc6c6
Giovana Zanoni <giovanacesario54@gmail.com>
2024-08-28 19:54:08 -0300

    conexao
    conectado com banco de dados

--- cont_usuario/controleUsuario.js
+++ cont_usuario/controleUsuario.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const db = require('../banco/conexao');
+const app = express();
+app.use(express.json());
 
 router.get("/login", (req, res) => {
     res.render("pages/login");
@@ -16,4 +19,26 @@ router.get("/inicialadmin",(req,res)=>{
 router.get("/inicialusuario",(req,res)=>{
     res.render("pages/inicialusuario")
 })
+
+// Rota de login
+app.post('/login', (req, res) => {
+  const { cpf, senha } = req.body;
+
+  if (!cpf || !senha) {
+    return res.status(400).json({ message: 'CPF e senha são obrigatórios.' });
+  }
+
+  db.query('SELECT * FROM usuario WHERE cpf_usuario = ? AND senha_usuario = ?', [cpf, senha], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: 'Erro ao consultar o banco de dados.' });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ message: 'CPF ou senha incorretos.' });
+    }
+
+    // Usuário autenticado com sucesso
+    res.json({ message: 'Login bem-sucedido!' });
+  });
+});
 module.exports = router;
