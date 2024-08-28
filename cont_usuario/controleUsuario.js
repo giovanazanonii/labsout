027db22244332ebb3519025312526f294ced7b7e
Giovana Zanoni <giovanacesario54@gmail.com>
2024-08-28 20:59:20 -0300

    autenticacao de login
    realizando autenticação de login, redirecionamento das paginas admin ou usuario normal, e modificação de senhas

--- cont_usuario/controleUsuario.js
+++ cont_usuario/controleUsuario.js
@@ -1,44 +1,86 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../banco/conexao');
-const app = express();
-app.use(express.json());
+const conexao = require('../banco/conexao');
+
+// Aplique os middlewares no router
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
 router.get("/login", (req, res) => {
     res.render("pages/login");
 });
-router.get("/modificarsenha",(req,res)=>{
-    res.render("pages/modificarsenha")
-})
-router.get("/telausuario",(req,res)=>{
-    res.render("pages/telausuario")
-})
-router.get("/inicialadmin",(req,res)=>{
-    res.render("pages/inicialadmin")
-})
-router.get("/inicialusuario",(req,res)=>{
-    res.render("pages/inicialusuario")
-})
+router.get("/modificarsenha", (req, res) => {
+    res.render("pages/modificarsenha");
+});
+router.get("/telausuario", (req, res) => {
+    res.render("pages/telausuario");3
+});
+router.get("/inicialadmin", (req, res) => {
+    res.render("pages/inicialadmin");
+});
+router.get("/inicialusuario", (req, res) => {
+    res.render("pages/inicialusuario");
+});
 
-// Rota de login
-app.post('/login', (req, res) => {
-  const { cpf, senha } = req.body;
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    const cpf = req.body.cpf;
+    const senha = req.body.senha;
 
-  if (!cpf || !senha) {
-    return res.status(400).json({ message: 'CPF e senha são obrigatórios.' });
+    const query = 'SELECT * FROM usuario WHERE cpf_usuario = ? AND senha_usuario = ?';
+
+    conexao.query(query, [cpf, senha], (error, results) => {
+        if (error) {
+            console.error('Erro ao executar a consulta: ' + error.stack);
+            res.status(500).send('Erro no servidor');
+            return;
+        } 
+        if (results.length > 0) {
+            const usuario = results[0];
+            //guardando o cpf do usuario para usar na modificação de senha
+            req.session.cpf = cpf;
+
+            if (usuario.tipo_usuario === 1) {
+              res.render("pages/modificarsenha");
+          } else {
+            (usuario.tipo_usuario === 2)
+              res.render("pages/inicialadmin");
+          }
+        } else {
+          res.render("pages/login", { errorMessage: 'CPF ou senha inválidos' });
+        }
+    });
+});
+
+router.post('/modificarsenha', (req, res) => {
+  const senha = req.body.senha;
+  const novasenha = req.body.novasenha;
+  const confirmarsenha = req.body.confirmarsenha;
+
+  //verificando se a novasenha é igual a confirmarsenha
+  if (novasenha !== confirmarsenha) {
+      return res.render("pages/modificarsenha", { errorMessage: 'As novas senhas não coincidem' });
   }
 
-  db.query('SELECT * FROM usuario WHERE cpf_usuario = ? AND senha_usuario = ?', [cpf, senha], (err, results) => {
-    if (err) {
-      return res.status(500).json({ message: 'Erro ao consultar o banco de dados.' });
-    }
+  const query = 'SELECT * FROM usuario WHERE cpf_usuario = ? AND senha_usuario = ?';
+  conexao.query(query, [req.session.cpf, senha], (error, results) => {
+      if (error) {
+          console.error('Erro ao executar a consulta: ' + error.stack);
+          return res.status(500).send('Erro no servidor');
+      }
+      if (results.length === 0) {
+          return res.render("pages/modificarsenha", { errorMessage: 'Senha atual incorreta' });
+      }
 
-    if (results.length === 0) {
-      return res.status(401).json({ message: 'CPF ou senha incorretos.' });
-    }
-
-    // Usuário autenticado com sucesso
-    res.json({ message: 'Login bem-sucedido!' });
+      const atualiza = 'UPDATE usuario SET senha_usuario = ? WHERE cpf_usuario = ?';
+      conexao.query(atualiza, [novasenha, req.session.cpf], (error) => {
+          if (error) {
+              console.error('Erro ao atualizar a senha: ' + error.stack);
+              return res.status(500).send('Erro no servidor');
+          }
+          res.redirect('/inicialusuario');
+      });
   });
 });
+
 module.exports = router;
